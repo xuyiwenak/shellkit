6d5f15bdb0f70c0f6d484aad4dccb1b7bbc81750
文余两人 <523272086@qq.com>
2018-05-17 09:50:41 +0800

    Update remoteLogin.sh

--- remoteLogin/remoteLogin.sh
+++ remoteLogin/remoteLogin.sh
@@ -2,7 +2,7 @@
 set timeout 30
 # 先把公钥放到服务器上,hostIp填写自己的ip, hostName填写用户名
 set hostIp "xxx.xxx.xxx.xxx"
-set hostName "evan"
+set hostName "username"
 spawn ssh  -i  /Users/tu/workstation/id_rsa.xuyiwen  $hostName@$hostIp
 interact
 exit
